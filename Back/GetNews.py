a3c400d3cbff19bedf9d020a15350fc2e51fbdb7
theisub <theisub1@gmail.com>
2019-04-16 22:25:27 +0300

    Added article parser which exports article into JSON structure

--- Back/GetNews.py
+++ Back/GetNews.py
@@ -1,8 +1,25 @@
 import requests
+import newspaper
+from newspaper import Article
+from newspaper.network import multithread_request
 import xml.etree.ElementTree as ET
 import json
+#TODO:Обработка ошибок
 
-
+def GetTxt(url,language ='ru'):
+    article = Article(url,language= language)
+    article.download()
+    try:
+        data = dict()
+        article.parse()
+        data['img'] = article.top_img
+        data['url'] = article.url
+        data['title'] = article.title
+        data['text']=article.text
+        data['description'] = article.meta_description # TODO: Адекватно сократить исходное тело новости до краткой аннотации
+        return data
+    except:
+        return ""
 
 def GetResponses(tag,limit = 'nolimit'):
     request = ('https://news.google.com/rss/search?q=%s&num=5&hl=ru&gl=RU&ceid=RU:ru' % (tag))
