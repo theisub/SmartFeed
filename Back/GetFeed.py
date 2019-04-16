488b26a950995e0b8f49c299ddc8461f944b08f7
theisub <theisub1@gmail.com>
2019-04-17 01:41:03 +0300

    Added function for forming short news feed for user. Ref #7

--- Back/GetFeed.py
+++ Back/GetFeed.py
@@ -12,3 +12,25 @@ def SortNewsByInterest(news):
     news.sort(key=lambda x: x['coef'], reverse=True)
     return news
 
+def FormUserFeed(news):
+    """
+    Формирование сокращенной ленты пользователя 
+    
+    @type news: list
+    @param news: Лента новостей
+
+    @rtype: dict
+    @return: Возвращает сокращенную ленту новостей, которые состоят из полей изображения, URL, заголовка и краткого описания 
+    """
+    feedkeys = ['img','url','title','description']
+    shortfeed = []
+    for article in news:
+        shortfeed.append({key:article[key] for key in feedkeys})
+
+    editedfeed = {
+        'size': len(shortfeed),
+        'articles' : shortfeed
+    }
+
+    return editedfeed
+
