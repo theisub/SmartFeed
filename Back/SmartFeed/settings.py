bca3092ed59f58bbbb1339aa60c998d6bc647e30
Salem Basim <theisub1@gmail.com>
2019-04-22 18:05:18 +0300

    Update settings.py

--- Back/SmartFeed/settings.py
+++ Back/SmartFeed/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'SmartFeed.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'news'
-        'HOST':'mongo',
+        'NAME': 'news',
+        'HOST': 'mongo'
     }
 }
 '''
