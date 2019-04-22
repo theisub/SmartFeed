a2a5d336cc53d7905db5c732278ff8a989ae9065
theisub <theisub1@gmail.com>
2019-04-22 03:48:11 +0300

    Modified comments to new style

--- Back/working_with_news/GetFeed.py
+++ Back/working_with_news/GetFeed.py
@@ -1,26 +1,22 @@
 
 def SortNewsByInterest(news):
-    """
+    """!
     Сортировка ленты новостей по коэффициенту интересности
     
-    @type news: list
-    @param news: Лента новостей
+    @param list news: Лента новостей
 
-    @rtype: list
-    @return: Возвращает ленту новостей отсортированной по полю 'coef'
+    @return dict: Возвращает ленту новостей отсортированной по полю 'coef'
     """
     news.sort(key=lambda x: x['coef'], reverse=True)
     return news
 
 def FormUserFeed(news):
-    """
+    """!
     Формирование сокращенной ленты пользователя 
     
-    @type news: list
-    @param news: Лента новостей
+    @param list news: Лента новостей
 
-    @rtype: dict
-    @return: Возвращает сокращенную ленту новостей, которые состоят из полей изображения, URL, заголовка и краткого описания 
+    @return dict: Возвращает сокращенную ленту новостей, которые состоят из полей изображения, URL, заголовка и краткого описания 
     """
     feedkeys = ['img','url','title','description']
     shortfeed = []
